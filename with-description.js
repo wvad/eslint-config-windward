0da77d99dbdd441d2d8f95703b87d1a9472380f8
csjsc <furandolltyan@gmail.com>
2022-04-09 11:03:44 +0900

    0.1.18

--- with-description.js
+++ with-description.js
@@ -31,6 +31,9 @@ module.exports = {
     "new-cap": "warn",
     "prefer-promise-reject-errors": "warn",
     "no-use-before-define": [ "warn", "nofunc" ],
+    "no-useless-constructor": "warn",
+    "no-lone-blocks": "warn",
+    "default-case-last": "warn",
 
 
     // Use regex leterals instead of RegExp constructor.
@@ -87,13 +90,11 @@ module.exports = {
     // Trailing spaces are not allowed.
     "no-trailing-spaces": "error",
 
-    // It is unnecessary to initialize variables with undefined.
-    // Veriables' default value is undefined.
-    "no-undef-init": "error",
+    // If a property has a getter and a setter, the setter should be defined right after the getter, or vice versa
+    "grouped-accessor-pairs": "error",
 
-    // Object.prorototype.hasOwnProperty.call is too long!
-    // Object.hasOwn is good.
-    "prefer-object-has-own": "error",
+    // Use camelcase.
+    "camelcase": [ "error", { ignoreDestructuring: true, ignoreImports: true, ignoreGlobals: true } ],
 
     // Use of the Array constructor to construct a new array is discouraged in favor of array literal notation.
     // It's because the single-argument pitfall and because the Array global may be redefined.
@@ -105,17 +106,14 @@ module.exports = {
     "no-iterator": "error",
     "no-caller": "error",
 
-    // These are format rules.
+    // Write as short and simple as possible.
+    "no-undef-init": "error",
+    "prefer-object-has-own": "error",
     "no-useless-computed-key": "error",
-    "no-useless-constructor": "error",
     "no-useless-return": "error",
-    "grouped-accessor-pairs": "error",
-    "no-lone-blocks": "error",
     "operator-assignment": "error",
     "prefer-exponentiation-operator": "error",
     "no-unneeded-ternary": "error",
-    "default-case-last": "error",
-    "camelcase": [ "error", { ignoreDestructuring: true, ignoreImports: true } ],
 
     // 'Array', 'Function' and 'RegExp' are constructors.
     // So those have to be call with 'new'.
@@ -133,21 +131,6 @@ module.exports = {
     }, {
       selector: "NewExpression[callee.name='BigInt']",
       message: "BigInt is not a constructor"
-    }],
-
-    // These should not be shadowed.
-    "id-denylist": [
-      "error", "Object", "Function", "Array", "Number","parseFloat", "parseInt", "Boolean", "String",
-      "Symbol", "Date", "Promise", "RegExp", "Error", "AggregateError", "EvalError", "RangeError",
-      "ReferenceError", "SyntaxError", "TypeError", "URIError", "globalThis", "JSON", "Math", "console",
-      "Intl", "ArrayBuffer", "Uint8Array","Int8Array", "Uint16Array", "Int16Array", "Uint32Array",
-      "Int32Array", "Float32Array", "Float64Array", "Uint8ClampedArray", "BigUint64Array", "BigInt64Array",
-      "DataView", "Map", "BigInt", "Set", "WeakMap", "WeakSet", "Proxy", "Reflect", "FinalizationRegistry",
-      "WeakRef", "decodeURI", "decodeURIComponent", "encodeURI", "encodeURIComponent", "escape",
-      "unescape", "isFinite", "isNaN", "atob", "btoa", "URL", "URLSearchParams", "DOMException", "TextEncoder",
-      "TextDecoder", "AbortController", "AbortSignal", "EventTarget", "Event", "MessageChannel", "MessagePort",
-      "MessageEvent", "clearInterval", "clearTimeout", "setInterval", "setTimeout", "queueMicrotask",
-      "performance", "structuredClone", "SharedArrayBuffer", "Atomics", "WebAssembly"
-    ],
+    }]
   }
 };
